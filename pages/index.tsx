f63a1da948f665e18d745bb2ef93aca9214058c5
alexMendes <alexifb1985@gmail.com>
2022-05-20 23:35:34 -0300

    feat: create project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
-import Banner from "../components/banner";
-import Card from "../components/card";
 import Navbar from "../components/navbar";
 import styles from "../styles/index.module.css";
 
 export default function Home() {
   return (
-    <div style={{ position: "relative" }}>
-      <div style={{ margin: "0" }}>
-        <Navbar title="gallery"></Navbar>
-      </div>
+    <div>
       <div>
-        <Banner />
-      </div>
-      <hr />
-      <div className={styles.card}>
-        <Card />
+        <Navbar title="gallery"></Navbar>
       </div>
     </div>
   );
